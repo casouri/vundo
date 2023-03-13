e6e414708f70889b32d19fa97c29a2958be260c9
JD Smith <93749+jdtsmith@users.noreply.github.com>
2023-03-13 16:12:43 -0400

    draw: avoid overwriting existing branch char and use compact space

--- vundo.el
+++ vundo.el
@@ -531,7 +531,10 @@ Translate according to `vundo-glyph-alist'."
                   (eq node (car (last (vundo-m-children parent)))))))
         ;; Go to parent.
         (if parent (goto-char (vundo-m-point parent)))
-        (let ((col (max 0 (1- (current-column)))))
+        (let ((col (max 0 (1- (current-column))))
+	      (room-for-another-rx
+	       (rx-to-string
+		`(or (>= ,(if vundo-compact-display 3 4) ?\s) eol))))
           (if (null parent)
               (insert (propertize (vundo--translate "○")
                                   'face 'vundo-node))
@@ -540,12 +543,13 @@ Translate according to `vundo-glyph-alist'."
               ;; Example: 1--2--3  Here we want to add a
               ;;             |     child to 1 but is blocked
               ;;             +--4  by that plus sign.
-              (while (not (looking-at (rx (or "    " eol))))
+              (while (not (looking-at room-for-another-rx))
                 (vundo--next-line-at-column col)
-                (unless (looking-at "$")
-                  (delete-char 1))
-                (insert (propertize (vundo--translate "│")
-                                    'face 'vundo-stem)))
+		;; do not overwrite existing branch
+                (when (looking-at (rx (or ?\s eol))) 
+		  (unless (looking-at "$") (delete-char 1))
+                  (insert (propertize (vundo--translate "│")
+                                      'face 'vundo-stem))))
               ;; Make room for inserting the new node.
               (unless (looking-at "$")
                 (delete-char (if vundo-compact-display 2 3)))
