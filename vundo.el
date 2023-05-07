ce986105a7819b288f57b381168605791aa4fcec
Yuan Fu <casouri@gmail.com>
2023-05-07 15:25:21 -0700

    * vundo.el: Bump version.

--- vundo.el
+++ vundo.el
@@ -5,7 +5,7 @@
 ;; Author: Yuan Fu <casouri@gmail.com>
 ;; Maintainer: Yuan Fu <casouri@gmail.com>
 ;; URL: https://github.com/casouri/vundo
-;; Version: 2.0.0
+;; Version: 2.1.0
 ;; Keywords: undo, text, editing
 ;; Package-Requires: ((emacs "28.1"))
 ;;
