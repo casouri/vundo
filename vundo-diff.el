ee52ba6b37777fee05a66f11bfca747a1435c15a
JD Smith <93749+jdtsmith@users.noreply.github.com>
2023-12-04 07:57:04 -0500

    tabs -> spaces

--- vundo-diff.el
+++ vundo-diff.el
@@ -55,15 +55,15 @@
 NODE defaults to the current node."
   (interactive)
   (let* ((mod-list vundo--prev-mod-list)
-	 (node (or node (vundo--current-node mod-list))))
+         (node (or node (vundo--current-node mod-list))))
     (setq vundo-diff--marked-node node)
     (unless vundo-diff--highlight-overlay
       (setq vundo-diff--highlight-overlay
-	    (make-overlay (1- (vundo-m-point node)) (vundo-m-point node)))
+            (make-overlay (1- (vundo-m-point node)) (vundo-m-point node)))
       (overlay-put vundo-diff--highlight-overlay
-		   'display (vundo--translate "●"))
+                   'display (vundo--translate "●"))
       (overlay-put vundo-diff--highlight-overlay
-		   'face 'vundo-diff-highlight)
+                   'face 'vundo-diff-highlight)
       (overlay-put vundo-diff--highlight-overlay 'priority 2))
     (move-overlay vundo-diff--highlight-overlay
                   (1- (vundo-m-point node))
@@ -84,64 +84,64 @@ Displays in a separate diff buffer with name based on
 the original buffer name."
   (interactive)
   (let* ((orig vundo--orig-buffer)
-	 (oname (buffer-name orig))
-	 (current (vundo--current-node vundo--prev-mod-list))
-	 (marked (or vundo-diff--marked-node (vundo-m-parent current)))
-	 (swapped (> (vundo-m-idx marked) (vundo-m-idx current)))
-	 dbuf)
+         (oname (buffer-name orig))
+         (current (vundo--current-node vundo--prev-mod-list))
+         (marked (or vundo-diff--marked-node (vundo-m-parent current)))
+         (swapped (> (vundo-m-idx marked) (vundo-m-idx current)))
+         dbuf)
     (if (or (not current) (not marked) (eq current marked))
-	(message "vundo diff not available.")
+        (message "vundo diff not available.")
       (let ((mrkbuf (get-buffer-create
-		     (make-temp-name (concat oname "-vundo-diff-marked")))))
-	(vundo--move-to-node current marked orig vundo--prev-mod-list)
-	(with-current-buffer mrkbuf
-	  (insert-buffer-substring-no-properties orig))
-	(vundo--refresh-buffer orig (current-buffer) 'incremental)
-	(vundo--move-to-node marked current orig vundo--prev-mod-list)
-	(vundo--refresh-buffer orig (current-buffer) 'incremental)
-	(setq dbuf (diff-no-select (if swapped orig mrkbuf)
-				   (if swapped mrkbuf orig)
-				   nil nil (get-buffer-create
-					    (concat "*vundo-diff-" oname "*"))))
-	(let* ((inhibit-read-only t)
-	       (a (if swapped current marked))
-	       (b (if swapped marked current))
-	       (diff-msg
-		(concat
-		 (propertize " vundo-diff: " 'font-lock-face 'diff-header)
-		 (propertize oname 'font-lock-face '(diff-file-header diff-header))
-		 "\n"
-		 (apply #'format "  %4s: %7s[%s]%s\n  %4s: %7s[%s]%s\n\n"
-			(cl-loop
-			 for x in (list a b) for h in '("From" "To")
-			 collect
-			 (propertize h 'font-lock-face 'diff-header)
-			 collect
-			 (propertize (if (eq x current) "Current"
-				       (if vundo-diff--marked-node
-					   "Marked" "Parent"))
-				     'font-lock-face
-				     (if (or (eq x current)
-					     (null vundo-diff--marked-node))
-					 'vundo-highlight 'vundo-diff-highlight))
-			 collect
-			 (propertize (number-to-string (vundo-m-idx x))
-				     'font-lock-face
-				     '(diff-file-header diff-header))
-			 collect
-			 (if-let* ((vundo-highlight-saved-nodes)
-				   (ts (vundo--mod-timestamp
-					vundo--prev-mod-list (vundo-m-idx x)))
-				   ((consp ts)))
-			     (propertize
-			      (format " Saved: %s" (format-time-string "%F %r" ts))
-			      'font-lock-face 'diff-header)
-			   ""))))))
-	  (with-current-buffer dbuf
-	    (goto-char (point-min))
-	    (insert diff-msg)))
-	(kill-buffer mrkbuf)
-	(display-buffer dbuf)))))
+                     (make-temp-name (concat oname "-vundo-diff-marked")))))
+        (vundo--move-to-node current marked orig vundo--prev-mod-list)
+        (with-current-buffer mrkbuf
+          (insert-buffer-substring-no-properties orig))
+        (vundo--refresh-buffer orig (current-buffer) 'incremental)
+        (vundo--move-to-node marked current orig vundo--prev-mod-list)
+        (vundo--refresh-buffer orig (current-buffer) 'incremental)
+        (setq dbuf (diff-no-select (if swapped orig mrkbuf)
+                                   (if swapped mrkbuf orig)
+                                   nil nil (get-buffer-create
+                                            (concat "*vundo-diff-" oname "*"))))
+        (let* ((inhibit-read-only t)
+               (a (if swapped current marked))
+               (b (if swapped marked current))
+               (diff-msg
+                (concat
+                 (propertize " vundo-diff: " 'font-lock-face 'diff-header)
+                 (propertize oname 'font-lock-face '(diff-file-header diff-header))
+                 "\n"
+                 (apply #'format "  %4s: %7s[%s]%s\n  %4s: %7s[%s]%s\n\n"
+                        (cl-loop
+                         for x in (list a b) for h in '("From" "To")
+                         collect
+                         (propertize h 'font-lock-face 'diff-header)
+                         collect
+                         (propertize (if (eq x current) "Current"
+                                       (if vundo-diff--marked-node
+                                           "Marked" "Parent"))
+                                     'font-lock-face
+                                     (if (or (eq x current)
+                                             (null vundo-diff--marked-node))
+                                         'vundo-highlight 'vundo-diff-highlight))
+                         collect
+                         (propertize (number-to-string (vundo-m-idx x))
+                                     'font-lock-face
+                                     '(diff-file-header diff-header))
+                         collect
+                         (if-let* ((vundo-highlight-saved-nodes)
+                                   (ts (vundo--mod-timestamp
+                                        vundo--prev-mod-list (vundo-m-idx x)))
+                                   ((consp ts)))
+                             (propertize
+                              (format " Saved: %s" (format-time-string "%F %r" ts))
+                              'font-lock-face 'diff-header)
+                           ""))))))
+          (with-current-buffer dbuf
+            (goto-char (point-min))
+            (insert diff-msg)))
+        (kill-buffer mrkbuf)
+        (display-buffer dbuf)))))
 
 ;;;###autoload
 (define-minor-mode vundo-diff-mode
